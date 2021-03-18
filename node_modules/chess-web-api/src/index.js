4f456359174fa3bbc2f52b497e2628da14182b77
SanjayMehra-27 <mehra271198@gmail.com>
2021-03-18 19:14:41 +0530

    Remove node_modules directory

--- node_modules/chess-web-api/src/index.js
+++
@@ -1,3 +0,0 @@
-const ChessWebAPI = require('./chess-web-api');
-
-module.exports = ChessWebAPI;
--- node_modules/chess-web-api/src/request/webapi-error.js
+++
@@ -1,9 +0,0 @@
-function WebapiError(message, statusCode) {
-  this.name = 'WebapiError';
-  this.message = message || '';
-  this.statusCode = statusCode;
-}
-
-WebapiError.prototype = Error.prototype;
-
-module.exports = WebapiError;
